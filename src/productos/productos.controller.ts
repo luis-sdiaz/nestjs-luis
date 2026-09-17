b5b71923ab0fce7fee8a2546309cdc9f87fa6365
Luis Sebastian Diaz <luis.diazyan@campusucc.edu.co>
2026-09-16 22:01:53 -0500

    feat: CRUD de usuarios y productos completado con validaciones

--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
 interface Producto {
   id: string;
@@ -13,11 +21,51 @@ interface Producto {
 @Controller('productos')
 export class ProductosController {
   private productos: Producto[] = [
-    { id: '1', name: 'Leche', price: 3.50, description: 'Leche entera 1L', stock: 10, vencido: false, categoria: 'Lácteos' },
-    { id: '2', name: 'Yogurt', price: 2.00, description: 'Yogurt de fresa', stock: 0, vencido: true, categoria: 'Lácteos' },
-    { id: '3', name: 'Pan', price: 1.50, description: 'Pan tajado', stock: 5, vencido: false, categoria: 'Panadería' },
-    { id: '4', name: 'Queso', price: 4.00, description: 'Queso campesino', stock: 0, vencido: false, categoria: 'Lácteos' },
-    { id: '5', name: 'Jamon', price: 5.00, description: 'Jamón de pavo', stock: 8, vencido: true, categoria: 'Embutidos' }
+    {
+      id: '1',
+      name: 'Leche',
+      price: 3500,
+      description: 'Leche entera 1L',
+      stock: 10,
+      vencido: false,
+      categoria: 'Lácteos',
+    },
+    {
+      id: '2',
+      name: 'Yogurt',
+      price: 2000,
+      description: 'Yogurt de fresa',
+      stock: 0,
+      vencido: true,
+      categoria: 'Lácteos',
+    },
+    {
+      id: '3',
+      name: 'Pan',
+      price: 1500,
+      description: 'Pan tajado',
+      stock: 5,
+      vencido: false,
+      categoria: 'Panadería',
+    },
+    {
+      id: '4',
+      name: 'Queso',
+      price: 4000,
+      description: 'Queso campesino',
+      stock: 0,
+      vencido: false,
+      categoria: 'Lácteos',
+    },
+    {
+      id: '5',
+      name: 'Jamon',
+      price: 5000,
+      description: 'Jamón de pavo',
+      stock: 8,
+      vencido: true,
+      categoria: 'Embutidos',
+    },
   ];
 
   // A. Listar Productos
@@ -42,7 +90,7 @@ export class ProductosController {
   @Get('categoria/:categoria')
   getByCategoria(@Param('categoria') categoria: string) {
     return this.productos.filter(
-      (p) => p.categoria.toLowerCase() === categoria.toLowerCase()
+      (p) => p.categoria.toLowerCase() === categoria.toLowerCase(),
     );
   }
 
@@ -50,5 +98,48 @@ export class ProductosController {
   @Get(':id')
   getById(@Param('id') id: string) {
     return this.productos.find((p) => p.id === id);
+  }
+  // Crear un Producto nuevo (POST)
+  @Post()
+  createProducto(@Body() nuevoProducto: Producto) {
+    this.productos.push(nuevoProducto);
+    console.log(
+      'Se ejecutó el método POST: Creando producto',
+      nuevoProducto.name,
+    );
+    return {
+      msg: 'Producto creado con éxito',
+      data: nuevoProducto,
+    };
+  }
+  // G. Actualizar un Producto (PUT)
+  @Put(':id')
+  updateProducto(@Param('id') id: string, @Body() cambiosProducto: any) {
+    const index = this.productos.findIndex((p) => p.id === id);
+    const productoActual = this.productos[index];
+
+    this.productos[index] = {
+      ...productoActual,
+      ...cambiosProducto,
+    };
+    console.log(`Se ejecutó el método PUT: Actualizando producto con ID ${id}`);
+
+    return {
+      msg: 'Producto actualizado con éxito',
+      data: this.productos[index],
+    };
+  }
+  // H. Eliminar un Producto (DELETE)
+  @Delete(':id')
+  deleteProducto(@Param('id') id: string) {
+    const index = this.productos.findIndex((p) => p.id === id);
+    this.productos.splice(index, 1);
+    console.log(
+      `Se ejecutó el método DELETE: Eliminando producto con ID ${id}`,
+    );
+
+    return {
+      msg: 'Producto eliminado con éxito',
+    };
   }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,47 +1,119 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { get } from 'http';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Put,
+} from '@nestjs/common';
 
-interface User{
-    id:string;
-    name: string;
-    email: string;
+interface User {
+  id: string;
+  name: string;
+  email: string;
 }
 @Controller('users')
 export class UsersController {
+  private users: User[] = [
+    { id: '1', name: 'Luis', email: 'sebaslsdy123@gmail.com' },
+    { id: '2', name: 'Ana', email: 'ana.martinez@gmail.com' },
+    { id: '3', name: 'Carlos', email: 'carlos.gomez@outlook.com' },
+    { id: '4', name: 'Sofia', email: 'sofia.rodriguez@hotmail.com' },
+    { id: '5', name: 'Mateo', email: 'mateo.fernandez@gmail.com' },
+    { id: '6', name: 'Valentina', email: 'valen.lopez@yahoo.com' },
+    { id: '7', name: 'Diego', email: 'diego.torres@gmail.com' },
+    { id: '8', name: 'Camila', email: 'camila.vargas@outlook.com' },
+    { id: '9', name: 'Javier', email: 'javi.morales@gmail.com' },
+    { id: '10', name: 'Isabella', email: 'isa.mendoza@gmail.com' },
+  ];
+  @Get('')
+  getAllUsers() {
+    return this.users;
+  }
+  @Get(':id')
+  getUserByid(@Param('id') id: string) {
+    console.log('.:: User ID: ', id);
+    const user = this.users.find((user) => user.id === id);
+    console.log('.:: usuario buscado: ', user);
 
-private users: User[] = [
-  { id: '1', name: 'Luis', email: 'sebaslsdy123@gmail.com' },
-  { id: '2', name: 'Ana', email: 'ana.martinez@gmail.com' },
-  { id: '3', name: 'Carlos', email: 'carlos.gomez@outlook.com' },
-  { id: '4', name: 'Sofia', email: 'sofia.rodriguez@hotmail.com' },
-  { id: '5', name: 'Mateo', email: 'mateo.fernandez@gmail.com' },
-  { id: '6', name: 'Valentina', email: 'valen.lopez@yahoo.com' },
-  { id: '7', name: 'Diego', email: 'diego.torres@gmail.com' },
-  { id: '8', name: 'Camila', email: 'camila.vargas@outlook.com' },
-  { id: '9', name: 'Javier', email: 'javi.morales@gmail.com' },
-  { id: '10', name: 'Isabella', email: 'isa.mendoza@gmail.com' }
-];
-
-    @Get('')
-    getAllUsers() {
-        return this.users;
+    if (user) {
+      return {
+        msg: 'Usuario encontrado',
+        data: user,
+      };
     }
-    @Get(':id')
-    getUserByid(@Param('id') id: string){
-        console.log('.:: User ID: ', id)
-        const user = this.users.find((user) => user.id == id);
-        console.log('.:: usuario buscado: ', user)
-        return user;
-
+    return {
+      msg: 'Usuario no encontrado',
+    };
+  }
+  @Get('search/:name')
+  getUserEmailByName(@Param('name') name: string) {
+    const data = this.users.find((user) => user.name === name);
+    if (data) {
+      return { result: data?.email };
+    } else {
+      return { result: 'User no encontrado' };
     }
-    @Get('search/:name')
-    getUserEmailByName(@Param('name') name: string) {
-        const data = this.users.find((user) => user.name === name);
-        if (data) {
-            return {result: data?.email};
-        } else {
-            return {result: 'User no encontrado'};
-        }
+  }
+  @Post()
+  createUser(@Body() userPayload: User) {
+    console.log('.:: user: ', userPayload);
+
+    const data = this.users.find(
+      (user) => user.id === userPayload.id || user.email === userPayload.email,
+    );
+
+    if (data) {
+      return {
+        msg: 'El usuario ya se encuentra registrado',
+      };
+    }
+    this.users.push(userPayload);
+    return {
+      msg: 'Usuario creado con éxito',
+      data: userPayload,
+    };
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    console.log('.:: UserID: ', id);
+    const position = this.users.findIndex((user) => user.id === id);
+    console.log('.:: position: ', position);
+    if (position === -1) {
+      return {
+        msg: 'No existe el ID',
+      };
+    }
+    this.users.splice(position, 1);
+
+    return {
+      msg: 'Usuario eliminado con exito',
+    };
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() userChanges: User) {
+    console.log(':: userID Update: ', id);
+    console.log(':: userChanges: ', userChanges);
+
+    const position = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      return {
+        msg: 'No existe el ID',
+      };
     }
 
+    const existingUser = this.users[position];
+    console.log(':: existingUser: ', existingUser);
+
+    const updatedUser = { ...existingUser, ...userChanges };
+    this.users[position] = updatedUser;
+
+    return {
+      msg: 'Usuario actualizado con éxito',
+      data: updatedUser,
+    };
+  }
 }
